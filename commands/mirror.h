77f33a3676efd89e192c82dd9763f65fe9a52d5b
Pietro Bonaldo <pietro.bogre@gmail.com>
2023-01-14 18:45:47 +0100

    deep region copy util

--- commands/mirror.h
+++ commands/mirror.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include "../bmp.h"
+#include "../utils.h"
 
 // makes a copy of the affected region, then mirrors
 // the original image by reading from the copy.
@@ -11,18 +12,8 @@
 void mirror_image24(IMAGE img24, int fx, int fy, int lx, int ly){
   int x_range = lx - fx + 1;
   int y_range = ly - fy + 1;
-
-
-  IMAGE img24_region_copy;
-  img24_region_copy.pixels = (RGB**) malloc(y_range*sizeof(void*));
-  for(int y = 0; y < y_range; y++){
-    img24_region_copy.pixels[y] = (RGB*) malloc(x_range*sizeof(RGB));
-    for(int x = 0; x < x_range; x++){
-      int original_x = x + fx;
-      int original_y = y + fy;
-      img24_region_copy.pixels[y][x] = img24.pixels[original_y][original_x];
-    }
-  }
+ 
+  RGB** deep_region_copy = deep_region_copy_img24(img24, fx, fy, lx, ly);
 
   for(int y = fy; y <= ly; y++){
     for(int x = fx; x <= lx; x++){
@@ -30,11 +21,11 @@ void mirror_image24(IMAGE img24, int fx, int fy, int lx, int ly){
       int copy_y = y - fy;
 
       int opposite_copy_x = (x_range-1)-copy_x;
-      img24.pixels[y][x] = img24_region_copy.pixels[copy_y][opposite_copy_x];
+      img24.pixels[y][x] = deep_region_copy[copy_y][opposite_copy_x];
     }
   }
 
   // free memory
-  for(int y = 0; y < y_range; y++) free(img24_region_copy.pixels[y]);
-  free(img24_region_copy.pixels);
+  for(int y = 0; y < y_range; y++) free(deep_region_copy[y]);
+  free(deep_region_copy);
 }
--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "bmp.h"
+#include "stdlib.h"
 
 RGB make_rgb(uint32_t red, uint32_t green, uint32_t blue){
   RGB rgb;
@@ -23,4 +24,22 @@ RGB make_rgb_mono(uint32_t color){
 int brightness_from_rgb(RGB rgb){
   int brightness = (rgb.R + rgb.G + rgb.B) / 3;
   return brightness;
+}
+
+RGB** deep_region_copy_img24(IMAGE img24, int fx, int fy, int lx, int ly){
+  int x_range = lx - fx + 1;
+  int y_range = ly - fy + 1;
+
+  RGB** deep_region_copy;
+  deep_region_copy = (RGB**) malloc(y_range*sizeof(void*));
+  for(int y = 0; y < y_range; y++){
+    deep_region_copy[y] = (RGB*) malloc(x_range*sizeof(RGB));
+    for(int x = 0; x < x_range; x++){
+      int original_x = x + fx;
+      int original_y = y + fy;
+      deep_region_copy[y][x] = img24.pixels[original_y][original_x];
+    }
+  }
+
+  return deep_region_copy;
 }
